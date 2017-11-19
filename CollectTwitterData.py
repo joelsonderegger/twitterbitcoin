132476514324f3df071d5016be6aa82c9b4eb205
joelsonderegger <joel.sonderegger@googlemail.com>
2017-11-19 21:07:27 +0100

    added template for script to collect twitter data

---
+++ CollectTwitterData.py
@@ -0,0 +1,18 @@
+# Script: CollectTwitterData.py
+# Authors: <insert name>
+
+""" This script gets tweets for a defined period that contain a certain word.
+	The output is a csv-file with the tweets.
+"""
+
+def Function():
+	""" Discription of function
+	"""
+
+
+# Begin the Python script that will do the work
+def main():
+
+
+if __name__ == '__main__':
+	main()

7e2a40c3e6f0522db4a5f0eb6b4038992a4821a4
alen.stepic <alen.stepic@student.unisg.ch>
2017-12-02 21:15:09 +0100

    adding csv

--- CollectTwitterData.py
+++ CollectTwitterData.py
@@ -19,29 +19,30 @@ import config
 import json
 import csv
 
-#Build and write a writer
-handle=csv.wrier(open("file.csv","wb"))
-def on_status
-
 #consumer key, consumer secret, access token, access secret.
 ckey="ANR2xCLJwM24NirkkFLlurkJY"
 csecret="KFAiqNSVErT7QzAlBYSkUmhGaCoEJJLnfmnG0x7SJJbDT2Qe1k"
 atoken="933214447291604992-xWBg0xEPzsWvrXSVTe5mWEr5o4SVH2n"
 asecret="DRJUfwWxk8MqKUZILnq8zu0pcsKahtTWZCKB64C822VQv"
 
+
 class listener(StreamListener):
 
-    def on_data(self, data):
-        print(data)
-        return(True)
-#how to print into a csv??
-		handle.wrierow(data)
 
-    def on_error(self, status):
-        print(status)
+ def on_data(self, data):
+	 try:
+		 print(data)
+		 saveFile = open('collectTwitterData.csv', 'a')
+		 saveFile.write(data)
+		 saveFile.write('\n')
+		 saveFile.close()
+		 return(True)
+ except BaseException, e:
+
+ def on_error(self, status):
+     print(status)
 
 auth = OAuthHandler(ckey, csecret)
 auth.set_access_token(atoken, asecret)
-
 twitterStream = Stream(auth, listener())
-twitterStream.filter(track=["car"])
+twitterStream.filter(track=["bitcoin"])

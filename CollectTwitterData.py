63d577e7967f26d3c2337362445ea96d0f3d66a4
joelsonderegger <joel.sonderegger@googlemail.com>
2017-12-03 13:14:04 +0100

    create structure so that every tweet is a dict that is appended in the csv-file

--- CollectTwitterData.py
+++ CollectTwitterData.py
@@ -2,7 +2,7 @@
 # Authors: Alen
 
 """ This script gets tweets for a defined period that contain a certain word.
-	The output is a csv-file with the tweets.
+    The output is a csv-file with the tweets.
  To use twitter APIs you have to open a twitteraccount on twitter application
 for the api credentials. A good tutorial can be found under
 https://pythonprogramming.net/twitter-api-streaming-tweets-python-tutorial/
@@ -27,19 +27,41 @@ asecret="DRJUfwWxk8MqKUZILnq8zu0pcsKahtTWZCKB64C822VQv"
 
 
 class listener(StreamListener):
+    def on_data(self, data):
+        # Twitter returns data in JSON format - we need to decode it first
+        decodedTweet = json.loads(data)
 
- def on_data(self, data):
-	 print(data)
-	 saveFile = open('data\collectTwitterData.csv', 'a')
-	 saveFile.write(data)
-	 saveFile.write('\n')
-	 saveFile.close()
-	 return(True)
+         # prints data for one tweet
+        print("===========")
 
- def on_error(self, status):
-     print(status)
+        # defines where the data is saved. Opening the file 'a' stands for appending
+        saveFile = open('data/twitterData.csv', 'a')
+
+        # gets relevant data from data object
+        created_at = decodedTweet['created_at']
+        text = decodedTweet['text'].encode('utf-8')
+
+        # Create a row that contains all relevant twitter data
+        tweet = [created_at, text]
+
+        # print out what is saved to the file
+        print(tweet)
+
+        # appends the tweet to the  csv-file
+        with saveFile:  
+           writer = csv.writer(saveFile)
+           writer.writerow(tweet)
+
+        #saveFile.write(tweet)
+        #saveFile.write('\n')
+        saveFile.close()
+        return(True)
+
+    def on_error(self, status):
+        print(status)
 
 auth = OAuthHandler(ckey, csecret)
 auth.set_access_token(atoken, asecret)
 twitterStream = Stream(auth, listener())
+
 twitterStream.filter(track=["bitcoin"])

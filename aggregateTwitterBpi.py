0900f711ebc7822326517f00055838057c3c88dd
joelsonderegger <joel.sonderegger@googlemail.com>
2017-12-29 18:35:42 +0100

    changed df var names

--- aggregateTwitterBpi.py
+++ aggregateTwitterBpi.py
@@ -22,53 +22,56 @@ def load_tweets():
 
 # Gets bpi data from CSV-File. Returns a data frame with bpi data.
 def load_bpi():
-    bpi_data = pd.read_csv('data/bpi.csv')
+    df_bpi = pd.read_csv('data/bpi.csv')
 
     # drop not needed columns
-    bpi_data.drop('average', axis=1, inplace=True)
-    bpi_data.drop('high', axis=1, inplace=True)
-    bpi_data.drop('low', axis=1, inplace=True)
+    df_bpi.drop('average', axis=1, inplace=True)
+    df_bpi.drop('high', axis=1, inplace=True)
+    df_bpi.drop('low', axis=1, inplace=True)
 
     # create new column 'close'. The close price is the open price of the previous hour (thus, shifting the open price -1 to get the close price)
-    bpi_data['close'] = bpi_data['open']
-    bpi_data['close'] = bpi_data['close'].shift(-1)
+    df_bpi['close'] = df_bpi['open']
+    df_bpi['close'] = df_bpi['close'].shift(-1)
+
+    # drop no more needed open column
+    df_bpi.drop('open', axis=1, inplace=True)
     
     # Set time as DataFrame index.
-    times = bpi_data.set_index(pd.DatetimeIndex(bpi_data['time']))
-    bpi_data = bpi_data.set_index([times.index.year, times.index.month, times.index.day, times.index.hour])
+    times = df_bpi.set_index(pd.DatetimeIndex(df_bpi['time']))
+    df_bpi = df_bpi.set_index([times.index.year, times.index.month, times.index.day, times.index.hour])
  
     # set correct index names for Y, M, D, H
-    bpi_data.index = bpi_data.index.set_names('Y', level=0)
-    bpi_data.index = bpi_data.index.set_names('M', level=1)
-    bpi_data.index = bpi_data.index.set_names('D', level=2)
-    bpi_data.index = bpi_data.index.set_names('H', level=3)
+    df_bpi.index = df_bpi.index.set_names('Y', level=0)
+    df_bpi.index = df_bpi.index.set_names('M', level=1)
+    df_bpi.index = df_bpi.index.set_names('D', level=2)
+    df_bpi.index = df_bpi.index.set_names('H', level=3)
 
-    return bpi_data
+    return df_bpi
 
 # Enriches BPI data frame with first difference and log first difference
 def first_difference_bpi(bpi_df):
 
-    # assigning the variable here to check later if there is a 'previous' btc close price.
-    prev_btc_close = None
+    # assigning the variable here to check later if there is a 'previous' bpi close price.
+    prev_bpi_close = None
 
     for index, row in bpi_df.iterrows():
         
-        # get btc closing price for index
-        this_btc_close = bpi_df.get_value(index, 'close', takeable=False)
+        # get bpi closing price for index
+        this_bpi_close = bpi_df.get_value(index, 'close', takeable=False)
 
         # prev_tw = DataFrame.get_value(index, 'close', takeable=False)
-        if prev_btc_close is not None:
+        if prev_bpi_close is not None:
 
-            # calculate the first difference for the btc closing price
-            first_difference_btc_close = this_btc_close - prev_btc_close
-            bpi_df.set_value(index, 'df_btc_close', first_difference_btc_close, takeable=False)
+            # calculate the first difference for the bpi closing price
+            first_difference_bpi_close = this_bpi_close - prev_bpi_close
+            bpi_df.set_value(index, 'df_bpi_close', first_difference_bpi_close, takeable=False)
 
-            # calculate the log first difference for the btc closing price
-            log_first_difference_btc_close = math.log(this_btc_close) - math.log(prev_btc_close)
-            bpi_df.set_value(index, 'log_df_btc_close', log_first_difference_btc_close, takeable=False)
+            # calculate the log first difference for the bpi closing price
+            log_first_difference_bpi_close = math.log(this_bpi_close) - math.log(prev_bpi_close)
+            bpi_df.set_value(index, 'log_df_bpi_close', log_first_difference_bpi_close, takeable=False)
 
         # set prev_tw for nex iteration
-        prev_btc_close = this_btc_close
+        prev_bpi_close = this_bpi_close
 
     return bpi_df
 
@@ -86,11 +89,11 @@ def first_difference_tweets(tweets_per_hour):
         # prev_tw = DataFrame.get_value(index, 'close', takeable=False)
         if prev_nr_of_tweets is not None:
 
-            # calculate the first difference for the btc closing price
+            # calculate the first difference for the bpi closing price
             first_difference_nr_of_tweets = this_nr_of_tweets - prev_nr_of_tweets
             tweets_per_hour.set_value(index, 'df_nr_of_tweets', first_difference_nr_of_tweets, takeable=False)
 
-            # calculate the log first difference for the btc closing price
+            # calculate the log first difference for the bpi closing price
             log_first_difference_nr_of_tweets = math.log(this_nr_of_tweets) - math.log(prev_nr_of_tweets)
             tweets_per_hour.set_value(index, 'log_df_nr_of_tweets', log_first_difference_nr_of_tweets, takeable=False)
 
@@ -130,8 +133,8 @@ def generate_csv(df_merged):
     # deleting the df values of the first row. A requirement for the following analysis.
     df_merged.ix[0,'df_nr_of_tweets'] = ""
     df_merged.ix[0,'log_df_nr_of_tweets'] = ""
-    df_merged.ix[0,'df_btc_close'] = ""
-    df_merged.ix[0,'log_df_btc_close'] = ""
+    df_merged.ix[0,'df_bpi_close'] = ""
+    df_merged.ix[0,'log_df_bpi_close'] = ""
 
     # This is the header for the list that contains nr of tweets and the bpi closing price for every hour
     header = ['time', 'nr_of_tweets', 'df_nr_of_tweets', 'log_df_nr_of_tweets', 'bpi_closing_price', 'df_bpi_closing_price', 'log_df_bpi_closing_price']
@@ -141,7 +144,7 @@ def generate_csv(df_merged):
     
     # loop through df_merged and appends a row that contains data for one hour to the data_array
     for index, row in df_merged.iterrows():
-        hourly_array = [row['time'], row['nr_tweets'], row['df_nr_of_tweets'], row['log_df_nr_of_tweets'], row['close'], row['df_btc_close'], row['log_df_btc_close']]
+        hourly_array = [row['time'], row['nr_tweets'], row['df_nr_of_tweets'], row['log_df_nr_of_tweets'], row['close'], row['df_bpi_close'], row['log_df_bpi_close']]
         data_array.append(hourly_array)
 
     # addind the header to the list which contains contains nr of tweets and the bpi closing price for every hour
@@ -158,30 +161,31 @@ def generate_csv(df_merged):
 def main():
 
     # load bitcoin price index (bpi) data from csv-file
-    bpi_data = load_bpi()
+    df_bpi = load_bpi()
 
     # enrich BPI data frame with first difference and log first difference
-    bpi_data = first_difference_bpi(bpi_data)
+    df_bpi = first_difference_bpi(df_bpi)
 
     # load tweets from csv-file
-    tweets = load_tweets()
+    df_tweets = load_tweets()
 
     # count number of tweets per hour
-    print('Counting the number of tweets. This can take some minutes...')
-    tweets_per_hour = count_tweets_per_hour(tweets)
+    print('Counting the number of tweets. This can take a while...')
+    df_tweets_per_hour = count_tweets_per_hour(df_tweets)
     print('Done counting the number of tweets.')
     
     # enrich tweets_per_hour data frame with first difference and log first difference
-    tweets_per_hour = first_difference_tweets(tweets_per_hour)
+    df_tweets_per_hour = first_difference_tweets(df_tweets_per_hour)
 
-    print(tweets_per_hour)
-    print(bpi_data)
+    print(df_tweets_per_hour)
+    print(df_bpi)
     # merge number of tweets per hour with bitcoin price index (bpi) data
-    df_merged = tweets_per_hour.join(bpi_data, how='left')
+    df_merged = df_tweets_per_hour.join(df_bpi, how='left')
     print('Tweets and BPI data is merged.')
 
     # Write out to csv
     generate_csv(df_merged)
+    print(df_merged)
     print("File successfully generated.")
 
 if __name__ == '__main__':
